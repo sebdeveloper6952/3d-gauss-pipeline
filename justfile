4a9b6364cb0869eeb2e9b756ae4c3f52e694330b
sebas <sebdeveloper6952@gmail.com>
2024-10-24 20:11:39 -0600

    wip

--- justfile
+++ justfile
@@ -3,3 +3,6 @@ build:
 
 build-linux:
     docker buildx build --platform linux/amd64 -t python-pipeline:$(git rev-parse HEAD) -t python-pipeline:latest .
+
+run:
+    poetry run python main.py
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pycolmap
+from pathlib import Path
+
+output_dir = Path("output/")
+image_dir = Path("./input")
+db_path = output_dir / "database.db"
+
+mvs_path = output_path / "mvs"
+database_path = output_path / "database.db"
+
+pycolmap.extract_features(database_path, image_dir)
+pycolmap.match_exhaustive(database_path)
+maps = pycolmap.incremental_mapping(database_path, image_dir, output_path)
+maps[0].write(output_path)
+
+# dense reconstruction
+pycolmap.undistort_images(mvs_path, output_path, image_dir)
+pycolmap.patch_match_stereo(mvs_path)  # requires compilation with CUDA
+pycolmap.stereo_fusion(mvs_path / "dense.ply", mvs_path)
